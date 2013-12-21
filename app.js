9c97e64bc9e9d615e1e82931e67b7f32969f907a
Adrian E <addis.aden@gmail.com>
2013-12-21 12:21:30 +0100

    search-engine is now an extra library

--- app.js
+++ app.js
@@ -3,73 +3,8 @@ var http = require('http'),
     repl = require('repl'),
     fs = require('fs'),
     querystring = require('querystring'),
-    url = require('url');
-
-var search = {
-  engines: (function() {
-    try {
-      return require('./search.json');
-    } catch(err) {
-      return {};
-    }
-  })(),
-  output: console.log,
-  pre: function(i) { return i },
-  get: function(s) {
-    return this.engines[s];
-  },
-  set: function(s, qs) {
-    this.engines[s] = qs;
-    fs.writeFile('./search.json', JSON.stringify(this.engines));
-  },
-  args: function(s) {
-    var engine = this.get(s);
-    if(engine) {
-      var m = engine.match(/%s/g);
-      if(!m) return 0;
-      return m.length;
-    } else {
-      this.output("Engine '" + s + "' is not defined.");
-      return null;
-    }
-  },
-  exec: function(s) {
-    var engine = this.get(s);
-    var arg_length = this.args(s);
-    if(!engine) {
-      return null;
-    }
-
-    if(arg_length === 0)
-      return engine;
-
-    if(arg_length !== (arguments.length - 1)) {
-      this.output("There have to be " + arg_length + " arguments for '" + s + "' engine.");
-      return null;
-    }
-
-    var result = '';
-    for(i = 0, q = engine.split("%s"); i < arg_length; i++) {
-      result = result.concat(q[i], this.pre(arguments[i + 1]));
-      if((i + 1) === arg_length)
-        result = result.concat(q[i + 1]);
-    }
-    return result;
-  },
-  qparse: function(query) {
-    var all = query.split(/\s+/);
-    var engine = all[0];
-    var rest = all.splice(1);
-    rest = rest.join(' ').replace(/^\s+/, '').replace(/\s+$/, '').split(/\s*,+\s*/);
-    if(rest.length > 0 && !(rest.length == 1 && '' === rest[0]))
-      return [engine].concat(rest);
-    else
-      return [engine];
-  },
-  q: function(query) {
-    return this.exec.apply(this, this.qparse(query));
-  }
-};
+    url = require('url'),
+    search = require('./lib/search.js');
 
 // run search.pre = querystring.escape for urls
 search.pre = querystring.escape;
